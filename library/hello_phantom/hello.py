36e456b66be4e097bad8235d419626188f674436
Joel W. King <joel.king@wwt.com>
2019-09-11 14:56:50 -0400

    verified hello will run outside the container

--- library/hello_phantom/hello.py
+++ library/hello_phantom/hello.py
@@ -7,47 +7,52 @@
 #     author: joel.king@wwt.com (@joelwking)
 #     written:  11 September 2019
 #
-#     description: hello.py
+#     description: Hello World skeleton program to demonstrate creating a container on Phantom Cyber
 #
-
+#
+# Import the SOAR class from the base_connector.py file
+#
 from base_connector import SOAR
-
+#
+# Optionally import user defined constants from hello_constants.py
+#
 try:
-    from hello_constants import *                          # file name would be ./hello_consts.py
-except Importerror                                         # the constants are optional
+    from hello_constants import *
+except ImportError:
     pass
+
 
 class Hello(SOAR):
 
-       BANNER = "HELLO"
+    BANNER = "HELLO"
 
     def __init__(self):
         """
-        Instance variables
+            Instance variables
         """
-        # Call the SOAR init first
-        super(Hello, self).__init__()
+
+        super(Hello, self).__init__()                      # Call the SOAR init first
+
 
     def handle_action(self):
         """
+            This method calls the 'msg' method from class SOAR in the base_connector. During initialization, the SOAR class 
+            has read the YAML configuration file to obtain the Phantom IP and token for authentication. 
+            We create and tage a container in Phantom, and exit.
         """
         self.msg('INFO: entering handle_action')
         phantom = self.create_phantom_object(self.args.get('phantom').get('public_ip'), self.args.get('phantom').get('ph_auth_token'))
 
-        container = dict(name='Cat9K', description='Code-for-Catalyst challenge', tags=['hello', 'demo'])
-        self.create_phantom_container(phantom, **container)
+        container = dict(name='Hello_World', description='Code-for-Catalyst challenge', tags=['hello', 'demo'])
+        self.create_phantom_container(phantom, container)
 
-
-# =============================================================================================
-# 
-# =============================================================================================
 
 if __name__ == '__main__':
 
     import sys
 
     connector = Hello()
-    connector.msg(connector.BANNER)
+    connector.msg('INFO: {}'.format(connector.BANNER))
     connector.handle_action()
 
-    exit(0)    
+    exit(0)
